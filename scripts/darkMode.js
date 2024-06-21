beb56ad47e2dfec981a568d9c8373030047d6ad3
Viktorfrosty <victormalpica0508@outlook.com>
2024-06-20 23:21:04 -0400

    home workbench

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -4,22 +4,30 @@ const cards = document.querySelectorAll(".card");
 
 modeButton.addEventListener("click", () => {
 	if (modeButton.textContent.includes("🌑")) {
-		body.style.background = "#000";
-		body.style.color = "#fff";
+		body.style.background = "var(--accent-color)";
+		body.style.color = "var(--font-altern-color)";
 		cards.forEach(card => {
+			card.style.border = "5px solid var(--secondary-color)";
 			card.style.boxShadow = "none";
 			card.querySelectorAll("a").forEach(link => {
-				link.style.color = "darkgray";
+				link.style.color = "var(--secondary-color)";
+			});
+			card.querySelectorAll("h2").forEach(h2 => {
+				h2.style.borderBottom = "5px solid var(--secondary-color)"
 			});
 		});
 		modeButton.textContent = "🌟";
 	} else {
 		body.style.background = "white";
-		body.style.color = "#000";
+		body.style.color = "var(--font-main-color)";
 		cards.forEach(card => {
+			card.style.border = "5px solid var(--accent-color)";
 			card.style.boxShadow = "5px 5px 25px var(--accent-color)";
 			card.querySelectorAll("a").forEach(link => {
-				link.style.color = "#1d5890";
+				link.style.color = "var(--main-color)";
+			});
+			card.querySelectorAll("h2").forEach(h2 => {
+				h2.style.borderBottom = "5px solid var(--accent-color)"
 			});
 		});
 		modeButton.textContent = "🌑";
