0b55764272c6305dcac24c820061eb9ad0f6dfa4
Viktorfrosty <victormalpica0508@outlook.com>
2024-06-26 11:44:19 -0400

    personal home page trial version

--- scripts/visitsCounter.js
+++ scripts/visitsCounter.js
@@ -2,12 +2,12 @@ const counter = document.querySelector("#counter");
 
 let visits = localStorage.getItem("numVisits") || 0;
 
-counter.textContent = `Visit count: ${visits}`;
+counter.textContent = `Page visit count: ${visits}`;
 
 visits++;
 
 localStorage.setItem("numVisits", visits);
 
 if (visits !== 0) {
-    counter.textContent = `Visit count: ${visits}`;
+    counter.textContent = `Page visit count: ${visits}`;
 }
