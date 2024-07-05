c49e36321afb3eb27d773527d6396b5b3a37224b
Viktorfrosty <victormalpica0508@outlook.com>
2024-07-04 22:49:46 -0400

    form trial version

---
+++ scripts/form.js
@@ -0,0 +1,21 @@
+const password = document.querySelector("#passsword1");
+
+const verification = document.querySelector("#verification");
+
+verification.addEventListener("focusout", checkSamePassword);
+
+function checkSamePassword() {
+
+    if (verification.value == "") {
+        verification.title = "The password needs to match.";
+    } else if (verification.value !== password.value) {
+        verification.title = "The passwords doesn't match.";
+        alert("The passwords doesn't match.")
+        password.value = "";
+        verification.value = "";
+        password.focus();
+    } else {
+        verification.title = "The passwords match.";
+    }
+
+}
