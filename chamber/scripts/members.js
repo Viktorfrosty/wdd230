0e345de4b35eafafc54390374d5845b4fb4ece3c
Viktorfrosty <victormalpica0508@outlook.com>
2024-07-11 23:58:39 -0400

    directory w.i.p.

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,3 +1,17 @@
 const baseURL = "https://viktorfrosty.github.io/wdd230/chamber/";
 
 const linksURL = "https://viktorfrosty.github.io/wdd230/chamber/data/members.json";
+
+const directory = document.querySelector("#directory");
+
+function prueba() {
+
+    const test = document.createElement("p");
+
+    test.textContent = "prueba";
+    
+    directory.append(test);
+
+};
+
+prueba();
