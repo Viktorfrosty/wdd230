2b963b8ec11e2fb610afe333a7b021ab0f79430b
Viktorfrosty <victormalpica0508@outlook.com>
2024-07-13 09:54:01 -0400

    directory w.i.p.

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -4,14 +4,218 @@ const linksURL = "https://viktorfrosty.github.io/wdd230/chamber/data/members.jso
 
 const directory = document.querySelector("#directory");
 
-function prueba() {
+function populateDirectory(members) {
 
-    const test = document.createElement("p");
+    let goldMemberlist = document.createElement("ul");
 
-    test.textContent = "prueba";
+    let npMemberlist = document.createElement("ul");
+
+    let silverMemberlist = document.createElement("ul");
+
+    let bronzeMemberlist = document.createElement("ul");
+
+    members.forEach(member => {
+        
+        let info = document.createElement("li");
+
+        let membershipLevel = document.createElement("h3");
+
+        let icon = document.createElement("img");
+
+        let name = document.createElement("h3");
+
+        let slogan = document.createElement("h4");
+
+        let categoriesHeader = document.createElement("p");
+
+        let categoriesList = document.createElement("ul");
+
+        let addressesHeader = document.createElement("p");
+
+        let addressesList = document.createElement("ul");
+
+        let numbersHeader = document.createElement("p");
+
+        let numbersList = document.createElement("ul");
+
+        let website = document.createElement("p");
+
+        let url = document.createElement("a");
+
+        if (member.membership == "gold") {
+
+            membershipLevel.textContent = `Gold Member`;
+            
+            membershipLevel.setAttribute("class", "membership-lvl gold")
+
+        } else if (member.membership == "np") {
+
+            membershipLevel.textContent = `Non Profit Member`;
+
+            membershipLevel.setAttribute("class", "membership-lvl np")
+
+        } else if (member.membership == "silver") {
+
+            membershipLevel.textContent = `Silver Member`;
+
+            membershipLevel.setAttribute("class", "membership-lvl silver")
+
+        } else if (member.membership == "bronze") {
+
+            membershipLevel.textContent = `Bronze Member`;
+
+            membershipLevel.setAttribute("class", "membership-lvl bronze")
+
+        };
+
+        icon.setAttribute("src", member.logo);
+
+        icon.setAttribute("alt", `${member.name} logo`);
+
+        icon.setAttribute("loading", "lazy");
+
+        icon.setAttribute("width", "100");
+
+        icon.setAttribute("height", "100");
+
+        name.textContent = `${member.name}`;
+
+        slogan.textContent = `${member.slogan}`;
+
+        categoriesHeader.textContent = `Services: `;
+
+        member.categories.forEach(category => {
+
+            let categoryName = document.createElement("li");
+
+            categoryName.textContent = `${category}`;
+            
+            categoriesList.appendChild(categoryName);
+            
+        });
+
+        if (member.addresses.length == 1) {
+
+            addressesHeader.textContent = `Address: `;
+
+        } else {
+
+            addressesHeader.textContent = `Addresses: `;
+
+        };
+
+        member.addresses.forEach(address => {
+
+            let addressInfo = document.createElement("li");
+
+            addressInfo.textContent = `${address}`;
+            
+            addressesList.appendChild(addressInfo);
+            
+        });
+
+        if (member.numbers.length == 1) {
+
+            numbersHeader.textContent = `Number: `;
+
+        } else {
+
+            numbersHeader.textContent = `Numbers: `;
+
+        };
+
+        member.numbers.forEach(number => {
+
+            let numberInfo = document.createElement("li");
+
+            numberInfo.textContent = `${number}`;
+            
+            numbersList.appendChild(numberInfo);
+            
+        });
+
+        website.textContent = `Website: `;
+
+        url.setAttribute("href", member.website);
+
+        url.setAttribute("target", "_blank");
+
+        url.textContent = `${member.name}`;
+
+        info.setAttribute("class", "info-card")
+
+        info.appendChild(membershipLevel);
+
+        info.appendChild(icon);
+
+        info.appendChild(name);
+
+        info.appendChild(slogan);
+
+        info.appendChild(categoriesHeader);
+
+        info.appendChild(categoriesList);
+
+        info.appendChild(addressesHeader);
+
+        info.appendChild(addressesList);
+
+        info.appendChild(numbersHeader);
+
+        info.appendChild(numbersList);
+
+        info.appendChild(website);
+
+        info.appendChild(url);
+
+        if (member.membership == "gold") {
+
+            goldMemberlist.append(info);
+
+        } else if (member.membership == "np") {
+
+            npMemberlist.append(info);
+
+        } else if (member.membership == "silver") {
+
+            silverMemberlist.append(info);
+
+        } else if (member.membership == "bronze") {
+
+            bronzeMemberlist.append(info);
+
+        }
+
+    });
     
-    directory.append(test);
+    goldMemberlist.setAttribute("id", "gold-members");
+
+    npMemberlist.setAttribute("id", "np-members");
+
+    silverMemberlist.setAttribute("id", "silver-members");
+
+    bronzeMemberlist.setAttribute("id", "bronze-members");
+
+    directory.appendChild(goldMemberlist);
+
+    directory.appendChild(npMemberlist);
+
+    directory.appendChild(silverMemberlist);
+
+    directory.appendChild(bronzeMemberlist);
 
 };
 
-prueba();
+async function getDirectory() {
+
+    let urlData = await fetch(linksURL);
+
+    let directoryData = await urlData.json();
+
+    console.log(directoryData.members);
+
+    populateDirectory(directoryData.members);
+
+};
+
+getDirectory();
