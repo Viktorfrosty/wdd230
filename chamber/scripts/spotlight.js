5dbc3dff3957fdfb915b49f984947a87629637b6
Viktorfrosty <victormalpica0508@outlook.com>
2024-07-22 01:34:39 -0400

    chamber w.i.p.

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -3,3 +3,29 @@ const baseURL = "https://viktorfrosty.github.io/wdd230/chamber/";
 const linksURL = "https://viktorfrosty.github.io/wdd230/chamber/data/members.json";
 
 let spotlight = document.querySelector("#spotlight-card");
+
+let counter = 1;
+
+async function getMembers() {
+    
+    let urlData = await fetch(linksURL);
+
+    let spotlightData = await urlData.json();
+
+    populateSpotlight(spotlightData.members);
+
+};
+
+function populateSpotlight(members) {
+
+    members.forEach(member => {
+
+        if (member.membership == "gold" || member.membership == "silver") {
+
+        };
+
+    });
+
+};
+
+getMembers();
