d7bb66b9c2e543d0b892cdeaaa811abfa61e5106
Viktorfrosty <victormalpica0508@outlook.com>
2024-07-23 09:13:59 -0400

    chamber trial version mod and fix

--- chamber/scripts/meeting.js
+++ chamber/scripts/meeting.js
@@ -6,11 +6,17 @@ const today = new Date().getDay();
 
 document.addEventListener('DOMContentLoaded', function() {
 
-    if (today === 1 || today === 2) {
+    if (today === 1) {
 
         popup.style.display = 'flex';
 
         invitation.textContent = "The Twilight's Chamber of Commerce helds a meet and greet gathering on Wednesday at 7:00 p.m.";
+
+    } else if (today === 2) {
+
+        popup.style.display = 'flex';
+
+        invitation.textContent = "The Twilight's Chamber of Commerce helds a meet and greet gathering tomorrow at 7:00 p.m.";
 
     } else if (today === 3) {
 
